c1a72811d38ebf6bf24c6496635a11a5885af483
AMELE | 038 <amirabas.merdas@gmail.com>
2026-06-05 23:41:59 +0330

    Update config.py

--- config.py
+++ config.py
@@ -15,6 +15,6 @@ WEBHOOK_PATH = f"/webhook/{BOT_TOKEN}"
 SECRET_TOKEN = os.getenv("SECRET_TOKEN", "SuperSecretToken1234567890")
 
 # آیدی گروه‌هایی که پست‌ها باید به آن‌ها فوروارد شوند (برای سیستم ویو)
-TARGET_VIEW_GROUPS = [-1001234567890, -1009876543210] 
+TARGET_VIEW_GROUPS = [-1003747486578, -1009876543210] 
 
 DB_URL = os.getenv("DATABASE_URL", "sqlite+aiosqlite:///bot.db")
